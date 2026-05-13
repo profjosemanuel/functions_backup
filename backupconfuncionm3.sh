b9d59336eb38b2d102a62debc86d6141b270918d
Dylan <ozidylan23@gmail.com>
2026-05-13 17:09:00 +0200

    prueba

--- backupconfuncionm3.sh
+++ backupconfuncionm3.sh
@@ -4,12 +4,17 @@ mkdir backup
 cd backup
 cp –r ${1} .
 tar –czvf backup.tar.gz *
-echo "He entrado en el bloque de la función"
-}
-mostrar_mensaje(){
+echo "Backup complete"
 }
 
+
+
+mostrar_mensaje() {
+echo "He entrado en el bloque de la función"
+}
+
+
 #A better approach is to use functions
-#perform_backup ${1}
+mostrar_mensaje
 
 
